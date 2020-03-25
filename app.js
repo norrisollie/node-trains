2b6e34ce63945c45ae43181612cb201bb44f454e
Ollie Norris <ollie.norris@adylic.com>
2020-03-25 09:23:29 +0000

    added train service cards to show time, destination, platform and operator

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const axios = require("axios").default;
 const parseString = require("xml2js").parseString;
+const stripPrefix = require("xml2js").processors.stripPrefix;
 require("dotenv").config();
+const csv = require("csv-parser");
+const fs = require("fs");
 
 const app = express();
 
@@ -15,57 +18,70 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 app.use(express.static("public"));
 
-// soap request for train data
-// const url =
-//   "https://realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01";
-// const args = {
-//   ["typ:TokenValue"]: process.env.nr_token
-// };
+const generateRequestEnvelope = (origin, destination) => {
+  console.log("creating soap request envelope");
 
-// soap.createClient(url, (err, client) => {
-//   client.addSoapHeader(
-//     "<soapenv:Header><typ:AccessToken><typ:TokenValue>" +
-//       process.env.nr_token +
-//       "</typ:TokenValue></typ:AccessToken></soapenv:Header>"
-//   );
+  let xml =
+    "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:typ='http://thalesgroup.com/RTTI/2013-11-28/Token/types' xmlns:ldb='http://thalesgroup.com/RTTI/2017-10-01/ldb/'>";
+  xml += "<soapenv:Header>";
+  xml += "<typ:AccessToken>";
+  xml += "<typ:TokenValue>" + process.env.nr_token + "</typ:TokenValue>";
+  xml += "</typ:AccessToken>";
+  xml += "</soapenv:Header>";
+  xml += "<soapenv:Body>";
+  xml += "<ldb:GetArrDepBoardWithDetailsRequest>";
+  xml += "<ldb:numRows>10</ldb:numRows>";
+  xml += "<ldb:crs>" + origin.toUpperCase() + "</ldb:crs>";
+  xml += "<ldb:filterCrs>" + destination.toUpperCase() + "</ldb:filterCrs>";
+  xml += "<ldb:filterType>to</ldb:filterType>";
+  xml += "<ldb:timeOffset>0</ldb:timeOffset>";
+  xml += "<ldb:timeWindow>120</ldb:timeWindow>";
+  xml += "</ldb:GetArrDepBoardWithDetailsRequest>";
+  xml += "</soapenv:Body>";
+  xml += "</soapenv:Envelope>";
 
-//   client.GetDepBoardWithDetails(args, (err, res) => {
-//     console.log(client);
-//   });
-// });
-xmls =
-  "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:typ='http://thalesgroup.com/RTTI/2013-11-28/Token/types' xmlns:ldb='http://thalesgroup.com/RTTI/2017-10-01/ldb/'>";
-xmls += "   <soapenv:Header>";
-xmls += "      <typ:AccessToken>";
-xmls +=
-  "         <typ:TokenValue>" + process.env.nr_token + "</typ:TokenValue>";
-xmls += "      </typ:AccessToken>";
-xmls += "   </soapenv:Header>";
-xmls += "   <soapenv:Body>";
-xmls += "      <ldb:GetArrDepBoardWithDetailsRequest>";
-xmls += "         <ldb:numRows>100</ldb:numRows>";
-xmls += "         <ldb:crs>LES</ldb:crs>";
-xmls += "         <ldb:filterCrs>FST</ldb:filterCrs>";
-xmls += "         <ldb:filterType>to</ldb:filterType>";
-xmls += "         <ldb:timeOffset>0</ldb:timeOffset>";
-xmls += "         <ldb:timeWindow>120</ldb:timeWindow>";
-xmls += "      </ldb:GetArrDepBoardWithDetailsRequest>";
-xmls += "   </soapenv:Body>";
-xmls += "</soapenv:Envelope>";
+  return xml;
+};
 
-axios
-  .post("https://realtime.nationalrail.co.uk/OpenLDBWS/ldb11.asmx", xmls, {
-    headers: { "Content-Type": "text/xml" }
-  })
-  .then(res => {
-    const data = res.data;
-    parseString(data, { trim: true }, function(err, res) {
-      console.log(res);
+const makeStationReq = (origin, destination, callback) => {
+  console.log("making station request");
+
+  axios
+    .post(
+      "https://realtime.nationalrail.co.uk/OpenLDBWS/ldb11.asmx",
+      generateRequestEnvelope(origin, destination),
+      {
+        headers: { "Content-Type": "text/xml" }
+      }
+    )
+    .then(res => {
+      parseString(
+        res.data,
+        {
+          tagNameProcessors: [stripPrefix],
+          explicitArray: false,
+          ignoreAttrs: true,
+          mergeAttrs: true
+        },
+        (err, result) => {
+          if (err) {
+            console.log("couldn't parse XML");
+          } else {
+            let data = result.Envelope.Body;
+            if (data.Fault) {
+              console.log("API Fault");
+            } else {
+              console.log("Request successfull");
+              callback(data);
+            }
+          }
+        }
+      );
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => {
-    console.log(err);
-  });
+};
 
 // home page
 app.get("/", (req, res) => {
@@ -75,16 +91,33 @@ app.get("/", (req, res) => {
 // form action
 app.get("/times", (req, res) => {
   const { origin, destination } = req.query;
-  res.redirect("/" + origin + "/" + destination);
+  // res.redirect("/" + origin + "/" + destination);
 });
 
 app.get("/:origin/:destination", (req, res) => {
   const { origin, destination } = req.params;
-  let showResults = true;
-  res.render("layouts/index", {
-    showResults: showResults,
-    origin: origin,
-    destination: destination
+
+  makeStationReq(origin, destination, data => {
+    const results =
+      data.GetArrDepBoardWithDetailsResponse.GetStationBoardResult;
+
+    try {
+      const messages = results.nrccMessages;
+      const services = results.trainServices.service;
+
+      let showResults = true;
+      res.render("layouts/index", {
+        showResults: showResults,
+        origin: origin,
+        destination: destination,
+        services: services
+      });
+    } catch (err) {
+      var error = true;
+      res.render("layouts/index", {
+        error: error
+      });
+    }
   });
 });
 
